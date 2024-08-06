3de340e8dd41f6f051d28f3c6bfa69b47ad2aef9
ConnorCritchley <critchleycm@appstate.edu>
2024-08-05 21:27:50 -0400

    Initial / Complete

---
+++ main.py
@@ -0,0 +1,8 @@
+print(input("Enter some text:"))
+print(float(input("Enter a number:")) + 1)
+print(float(input("Enter a number:")) + .5)
+
+print("The sum is:", float(input("Enter a number:")) + float(input("Enter another number:")))
+print("The product is:", float(input("Enter a number:")) * float(input("Enter another number:")))
+print("The result is:", float(input("Enter a number:")) / float(input("Enter another number:")))
+
